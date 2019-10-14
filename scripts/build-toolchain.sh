d3d4fe8df9ce0f7eea66a802f57ddf212f854ba4
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-10-14 22:44:50 +0200

    check existence of files before downloading

--- scripts/build-toolchain.sh
+++ scripts/build-toolchain.sh
@@ -9,23 +9,51 @@ fi
 
 PREFIX_DIR=$1
 
-wget https://sunsite.icm.edu.pl/pub/gnu/binutils/binutils-2.32.tar.xz
-wget https://sunsite.icm.edu.pl/pub/gnu/gcc/gcc-9.2.0/gcc-9.2.0.tar.xz
-wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2
-tar xvf binutils-2.32.tar.xz
-tar xvf gcc-9.2.0.tar.xz
-tar xvf avr-libc-2.0.0.tar.bz2
+DIR_BINUTILS=binutils-2.32
+FILE_BINUTILS=$DIR_BINUTILS.tar.xz
+
+DIR_GCC=gcc-9.2.0
+FILE_GCC=$DIR_GCC.tar.xz
+
+DIR_LIBC=avr-libc-2.0.0
+FILE_LIBC=$DIR_LIBC.tar.bz2
+
+if [ ! -f $FILE_BINUTILS ]; then
+    wget https://sunsite.icm.edu.pl/pub/gnu/binutils/$FILE_BINUTILS
+fi
+
+if [ ! -f $FILE_GCC ]; then
+    wget https://sunsite.icm.edu.pl/pub/gnu/gcc/gcc-9.2.0/$FILE_GCC
+fi
+
+if [ ! -f $FILE_LIBC ]; then
+    wget http://download.savannah.gnu.org/releases/avr-libc/$FILE_LIBC
+fi
+
+if [ ! -d $DIR_BINUTILS ]; then
+    tar xvf $FILE_BINUTILS
+fi
+
+if [ ! -d $DIR_GCC ]; then
+    tar xvf $FILE_GCC
+fi
+
+if [ ! -d $DIR_LIBC ]; then
+    tar xvf $FILE_LIBC
+fi
 
 mkdir -p build/binutils
 mkdir -p build/gcc
+mkdir -p build/libc
+
 cd build/binutils
-../../binutils-2.32/configure --prefix=$PREFIX_DIR --target=avr --disable-nls
+../../$DIR_BINUTILS/configure --prefix=$PREFIX_DIR --target=avr --disable-nls
 
 # TODO: run gmake on FreeBSD
 make -j4
 make install
 cd ../gcc
-../../gcc-9.2.0/configure --prefix=$PREFIX_DIR --target=avr --enable-languages=c,c++ \
+../../$DIR_GCC/configure --prefix=$PREFIX_DIR --target=avr --enable-languages=c,c++ \
     --disable-nls --disable-libssp --with-dwarf2
 make -j4
 make install
