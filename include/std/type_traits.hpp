1b37e0ec2adc033a82a02b6e5440f26ceb9833f0
Łukasz Gemborowski <lukasz.gemborowski@gmail.com>
2020-01-26 15:31:29 +0100

    add remove_reference_t wrapper

--- include/std/type_traits.hpp
+++ include/std/type_traits.hpp
@@ -22,6 +22,9 @@ template< class T > struct remove_reference      {typedef T type;};
 template< class T > struct remove_reference<T&>  {typedef T type;};
 template< class T > struct remove_reference<T&&> {typedef T type;};
 
+template<class T>
+using remove_reference_t = typename remove_reference<T>::type;
+
 // --------------------------------------------------------------
 // https://en.cppreference.com/w/cpp/types/conditional
 template<bool B, class T, class F>
